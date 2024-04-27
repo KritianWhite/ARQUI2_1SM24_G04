fb7fa99e414c3e6c28a8d14387686f702bba0565
Douglas <dougdari@gmail.com>
2024-04-27 04:44:35 -0600

    ultimos archivos

---
+++ Fase 3/main/sql/scriptArqui2.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE sensores;
+USE sensores;
+
+
+CREATE TABLE iluminacion (
+    valor FLOAT,
+    tiempo DATETIME
+);
+
+
+CREATE TABLE proximidad (
+    valor INT,
+    tiempo DATETIME
+);
+
+
+CREATE TABLE aire (
+    valor FLOAT,
+    tiempo DATETIME
+);
+
+CREATE TABLE temperatura (
+    valor FLOAT,
+    tiempo DATETIME
+);
+
+
+CREATE TABLE humedad (
+    valor FLOAT,
+    tiempo DATETIME
+);
+
+
+
+
+
+CREATE TABLE seguridad (
+    valor FLOAT,
+    tiempo DATETIME
+);
+
+
